a8548ca7a9370934e795f7ad3d93d382cc1bd782
mhsohail <sohailx2x@yahoo.com>
2015-04-04 23:50:07 +0500

    a

--- index.php
+++ index.php
@@ -3,4 +3,14 @@
 $vendor = Mage::getSingleton('udropship/session');
 $vendor_id = $vendor->getVendorId();
 
+// get vendors data from database
+$vendors = Mage::getModel('udropship/vendor')->getCollection();
+echo "<pre>";
+foreach($vendors as $vendor) {
+	$ven = Mage::getModel('udmember/membership')->load($vendor->getId());
+	$data = $vendor->getData();
+	echo $data['vendor_type'];
+	//restaurant_merchant
+}
+echo "</pre>";
 ?>
